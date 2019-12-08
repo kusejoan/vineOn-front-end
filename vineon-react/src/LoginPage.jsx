79c47a3e95e826bd9b904fa37a8e3847a05f2cbb
joasia <p>
2019-12-08 22:52:29 +0100

    user service

--- vineon-react/src/LoginPage.jsx
+++ vineon-react/src/LoginPage.jsx
@@ -1,43 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { UserService } from "./user.service";
 
+const login = (username, password) => {
+  const response = UserService().login(username, password);
+  response.then(value => console.log(value)).catch(error => console.log(error)) 
+};
 
-const handleOnChange = (field,component) => (event) => {
-    component.setState({[field]: event.target.value})
-}
-    
-export class LoginPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        username: "",
-        password: ""
-    };
-    this.handleUsernameChange = handleOnChange("username", this)
-    this.handlePasswordChange = handleOnChange("password", this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
+export const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  handleSubmit(event){
-      console.log(this.state)
-      event.preventDefault()
-  }
-
-  render() {
-    const {username, password} = this.state  
-    return (
-      <React.Fragment>
-        <form onSubmit={this.handleSubmit}>
-          <fieldset>
-            <p>
-              username: <input type="text" onChange={this.handleUsernameChange} value={username} />{" "}
-            </p>
-            <p>
-              password: <input type="text" onChange={this.handlePasswordChange} value={password}/>{" "}
-            </p>
-            <input type="submit" value="Submit" />
-          </fieldset>
-        </form>
-      </React.Fragment>
-    );
-  }
-}
+  return (
+    <React.Fragment>
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          login(username, password);
+        }}
+      >
+        <fieldset>
+          <p>
+            username:
+            <input
+              type="text"
+              onChange={event => setUsername(event.target.value)}
+              value={username}
+            />
+          </p>
+          <p>
+            password:
+            <input
+              type="text"
+              onChange={event => setPassword(event.target.value)}
+              value={password}
+            />
+          </p>
+          <input type="submit" value="Submit" />
+        </fieldset>
+      </form>
+    </React.Fragment>
+  );
+};
