4eb3b4dfd9462453f1082c19c0fd7a78cb28965c
Filip Jachura <filjac5882@op.pl>
2020-01-17 03:02:45 +0100

    Sessions finally work

--- vineon-react/src/User/user.service.js
+++ vineon-react/src/User/user.service.js
@@ -1,8 +1,8 @@
 import Axios from "axios";
+import qs from 'qs';
 import { API_URL, APP_JSON } from "../api";
 
 // POST JSON
-
 const loginUrl = `${API_URL}/login`;
 const userRegisterUrl = `${API_URL}/register`;
 const storeUpdateUrl = `${API_URL}/user/store/update`;
@@ -11,13 +11,13 @@ const storesOfWineUrl = `${API_URL}/user/storesofwine`;
 const addWineUrl = `${API_URL}/user/addwine`;
 const allWinesUrl = `${API_URL}/user/getAllWines`;
 
-
+Axios.defaults.withCredentials = true;
 export const UserService = () => ({
   async login(JSESSINID, username, password) {
     console.log(loginUrl);
     const response = await Axios.post(loginUrl, {
       headers: { Accept: APP_JSON },
-      params: { JSESSINID, username, password }
+      params: { username, password }
     });
 
     return response.data;
@@ -42,13 +42,13 @@ export const UserService = () => ({
 
     return response.data;
   },
-
+//na tym trzeba wzorować pozostałe serwisy - można wywalić stąd te JSESSINID i headers
   async updateUser(JSESSINID, firstName, surname, birthdate) {
     console.log(userUpdateUrl);
-    const response = await Axios.post(userUpdateUrl, {
+    const response = await Axios.post(userUpdateUrl, qs.stringify({
       headers: { Accept: APP_JSON },
       params: { JSESSINID, firstName, surname, birthdate }
-    });
+    }));
 
     return response.data;
   },
